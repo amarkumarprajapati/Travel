ff1733a0e9256331562e30d086ba57814e4f403f
Amar Kumar Prajapati <100253987+amarkumarprajapati@users.noreply.github.com>
2023-11-17 14:26:29 +0530

    main

---
+++ src/Travel.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Hero from './Components/Hero'
+import Navbar from './Components/Navbar'
+import About from './Components/About'
+import Service from './Components/Service'
+import Section2 from './Components/Section2'
+import Contact from './Components/Contact'
+
+const Travel = () => {
+  return (
+    <>
+    <Navbar />
+    <Hero />
+    <About />
+    <Service />
+    <Section2 />
+    <Contact />
+    </>
+  )
+}
+
+export default Travel
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import Website4 from './website4'
+import Travel from './Travel'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Website4 />
+    <Travel/>
   </React.StrictMode>,
 )
